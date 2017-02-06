93820c8793209fcf79cf62425d0ed25df88bc4c1
Egor Malykh <fnk@fea.st>
2017-02-06 20:31:39 +0300

    base QGate class

--- quantum.py
+++ quantum.py
@@ -19,3 +19,16 @@ class QRegister:
         assert isinstance(gate, QGate)
         assert self._n == gate._n
         self._data = gate._data @ self._data
+
+class QGate:
+    def __init__(self, matrix):
+        self._data = np.array(matrix, dtype=np.complex64)
+
+        assert len(self._data.shape) == 2
+        assert self._data.shape[0] == self._data.shape[1]
+
+        self._n = np.log2(self._data.shape[0])
+
+        assert self._n.is_integer()
+
+        self._n = int(self._n)
