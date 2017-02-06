58a6ffe65f20adc2e320ccaeb7a2fc162ac53dc0
Egor Malykh <fnk@fea.st>
2017-02-06 20:30:59 +0300

    measure function

--- quantum.py
+++ quantum.py
@@ -8,3 +8,9 @@ class QRegister:
 
         self._data = np.zeros((2 ** self._n), dtype=np.complex64)
         self._data[int('0b' + init, 2)] = 1
+
+    def measure(self):
+        probs = np.real(self._data) ** 2 + np.imag(self._data) ** 2
+        states = np.arange(2 ** self._n)
+        mstate = np.random.choice(states, size=1, p=probs)[0]
+        return f'{mstate:>0{self._n}b}'
