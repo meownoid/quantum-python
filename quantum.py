b8688e67f6508c5a8233c0081af07d39c5d6197c
Egor Malykh <fnk@fea.st>
2017-02-06 20:32:29 +0300

    base gates

--- quantum.py
+++ quantum.py
@@ -44,3 +44,10 @@ class QGate:
             x = np.kron(x, self._data)
 
         return QGate(x)
+
+
+I = QGate([[1, 0], [0, 1]])
+H = QGate(np.array([[1, 1], [1, -1]]) / np.sqrt(2))
+X = QGate([[0, 1], [1, 0]])
+Y = QGate([[0, -1j], [1j, 0]])
+Z = QGate([[1, 0], [0, -1]])
