b96da84ed96bc82b3c69157382836b759a3c9550
Egor Malykh <fnk@fea.st>
2017-02-06 20:32:50 +0300

    U gate

--- quantum.py
+++ quantum.py
@@ -51,3 +51,27 @@ H = QGate(np.array([[1, 1], [1, -1]]) / np.sqrt(2))
 X = QGate([[0, 1], [1, 0]])
 Y = QGate([[0, -1j], [1j, 0]])
 Z = QGate([[1, 0], [0, -1]])
+
+
+def U(f, n):
+    m = n + 1
+
+    U = np.zeros((2**m, 2**m), dtype=np.complex64)
+
+    def bin2int(xs):
+        r = 0
+        for i, x in enumerate(reversed(xs)):
+            r += x * 2 ** i
+        return r
+
+    for xs in product({0, 1}, repeat=m):
+        x = xs[:~0]
+        y = xs[~0]
+
+        z = y ^ f(*x)
+
+        instate = bin2int(xs)
+        outstate = bin2int(list(x) + [z])
+        U[instate, outstate] = 1
+
+    return QGate(U)
